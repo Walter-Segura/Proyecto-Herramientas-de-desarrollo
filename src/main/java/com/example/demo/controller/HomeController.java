0f6ce5f9eb29d03de08f3207cee76f3dc582934b
Walter-Segura <u22217578@utp.edu.pe>
2025-11-20 23:46:16 -0500

    app en la nube

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -2,8 +2,11 @@ package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
+@RequestMapping("/front")
 public class HomeController {
 
     @GetMapping("/index")
